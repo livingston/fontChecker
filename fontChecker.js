77c1c972fd749fb496a909bba3f44bc0be076a41
Livingston Samuel <livingston.samuel@gmail.com>
2011-02-11 14:29:03 +0530

    fixed wrong detection of "Osmanya", add minified version

--- fontChecker.js
+++ fontChecker.js
@@ -4,68 +4,70 @@
  * Based on idea and implementation of Alex Young, http://dailyjs.com/2010/05/12/language-support
  */
 (function (W, D) {
-  var fontChecker = (function () {
-    var span = D.createElement('span'),
-        div = D.createElement('div'),
-        body = D.body,
-        firstChild = body.firstChild,
-            
-      langs = {
-        'arabic': ['\u06c2', '\u06d2'],
-        'avestan': ['\u10b00', '\u10b01'],
-        'bamum': ['\ua6f3', '\ua6f4'],
-        'bengali': ['\u0985', '\u0986'],
-        'carian': ['\u102a2', '\u102c5'],
-        'cryllic': ['\u0402', '\u04b1'],
-        'devanagri': ['\u0905', '\u0964'],
-        'ethiopic': ['\u1220', '\u1221'],
-        'georgian': ['\u10a4', '\u10a5'],
-        'glagolitic': ['\u2c15', '\u2c45'],
-        'gothic': ['\u10338', '\u10339'],
-        'greek': ['\u03e2', '\u03e3'],
-        'gujarati': ['\u0a85', '\u0a86'],
-        'hebrew': ['\u05df', '\u05e3'],
-        'hieroglyphs': ['\u130d0', '\u130d1'],
-        'japanese': ['\uff71', '\u30a2'],
-        'kannada': ['\u0c89', '\u0c90'],
-        'lao': ['\u0ec0', '\u0ec1'],
-        'latin': ['\u004c', '\u006c'],
-        'malayalam': ['\u0d05', '\u0d06'],
-        'mongolian': ['\u1830', '\u1831'],
-        'oriya': ['\u0b05', '\u0b06'],
-        'osmanya': ['\u1049c', '\u104a9'],
-        'syriac': ['\u0712', '\u0713'],
-        'tamil': ['\u0b85', '\u0bf5'],
-        'telugu': ['\u0c18', '\u0d18'],
-        'thai': ['\u0e12', '\u0e40'],
-        'tibetan': ['\u0f11', '\u0f12']
-      };
+    var fontChecker = (function () {
+        var span = D.createElement('span'),
+            div = D.createElement('div'),
+            body = D.body,
+            firstChild = body.firstChild,
 
-    return function (lang) {
-      if (!(lang in langs)) {
-        return false;
-      }
+            langs = {
+                'arabic': ['\u06c2', '\u06d2'],
+                'avestan': ['\u10b00', '\u10b01'],
+                'bamum': ['\ua6f3', '\ua6f4'],
+                'bengali': ['\u0985', '\u0986'],
+                'carian': ['\u102a2', '\u102c5'],
+                'cryllic': ['\u0402', '\u04b1'],
+                'devanagri': ['\u0905', '\u0964'],
+                'ethiopic': ['\u1220', '\u1221'],
+                'georgian': ['\u10a4', '\u10a5'],
+                'glagolitic': ['\u2c15', '\u2c45'],
+                'gothic': ['\u10338', '\u10339'],
+                'greek': ['\u03e2', '\u03e3'],
+                'gujarati': ['\u0a85', '\u0a86'],
+                'hebrew': ['\u05df', '\u05e3'],
+                'hieroglyphs': ['\u130d0', '\u130d1'],
+                'japanese': ['\uff71', '\u30a2'],
+                'kannada': ['\u0c89', '\u0c90'],
+                'lao': ['\u0ec0', '\u0ec1'],
+                'latin': ['\u004c', '\u006c'],
+                'malayalam': ['\u0d05', '\u0d06'],
+                'mongolian': ['\u1830', '\u1831'],
+                'oriya': ['\u0b05', '\u0b06'],
+                'osmanya': ['\u10481', '\u10483'],
+                'syriac': ['\u0712', '\u0713'],
+                'tamil': ['\u0b85', '\u0bf5'],
+                'telugu': ['\u0c18', '\u0d18'],
+                'thai': ['\u0e12', '\u0e40'],
+                'tibetan': ['\u0f11', '\u0f12']
+            };
 
-      var selectedLang = langs[lang],
-          charA = span.cloneNode(false),
-          charB = span.cloneNode(false),
-          holder = div.cloneNode(false),
-          fontExists = false;
+        return function (lang) {
+            if (!(lang in langs)) {
+                return false;
+            }
 
-      charA.appendChild(D.createTextNode(selectedLang[0]));
-      charB.appendChild(D.createTextNode(selectedLang[1]));
+            var selectedLang = langs[lang],
+                charA = span.cloneNode(false),
+                charB = span.cloneNode(false),
+                holder = div.cloneNode(false),
+                fontExists = false;
 
-      holder.appendChild(charA);
-      holder.appendChild(charB);
+            charA.appendChild(D.createTextNode(selectedLang[0]));
+            charB.appendChild(D.createTextNode(selectedLang[1]));
 
-      body.insertBefore(holder, firstChild);
+            holder.appendChild(charA);
+            holder.appendChild(charB);
 
-      fontExists = charA.offsetWidth !== charB.offsetWidth;
-      body.removeChild(holder);
+            body.insertBefore(holder, firstChild);
 
-      return fontExists;
-    };
-  }());
-  
-  W.fontChecker = fontChecker;
+            fontExists = charA.offsetWidth !== charB.offsetWidth;
+
+            body.removeChild(holder);
+            holder = charA = charB = null;
+
+            return fontExists;
+        };
+    }());
+
+    W.fontChecker = fontChecker;
 }(window, document));
